2a9e9e32d958c50e61d0c59c755651d91f4da48d
Janetta Kattoeva <jkattoeva@gmail.com>
2024-02-07 18:33:06 +0600

    add more styles for the card

--- src/app/components/card/Card.jsx
+++ src/app/components/card/Card.jsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
-import { SearchIcon } from "@/app/components/card/index";
+import {
+  SearchIcon,
+  HumidityIcon,
+  WindIcon,
+  rain,
+} from "@/app/components/card/index";
 import cls from "./Card.module.scss";
 
 export default function CardPage() {
@@ -17,7 +22,28 @@ export default function CardPage() {
             <Image src={SearchIcon} alt="search icon" />
           </button>
         </div>
-        <div className={cls.weather}></div>
+        <div className={cls.weather}>
+          <Image src={rain} alt="weather icon" className={cls.weather_icon} />
+          <h1 className={cls.temp}>−6°</h1>
+          <h2 className={cls.city}>New York</h2>
+
+          <div className={cls.details}>
+            <div className={cls.col}>
+              <Image src={HumidityIcon} alt="humidity" width={30} />
+              <div>
+                <p className={cls.humidity}>50%</p>
+                <p>Humidity</p>
+              </div>
+            </div>
+            <div className={cls.col}>
+              <Image src={WindIcon} alt="wind" width={30} />
+              <div>
+                <p className={cls.wind}>15 km/h</p>
+                <p>Wind Speed</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/app/components/card/index.js
+++ src/app/components/card/index.js
@@ -1,1 +1,4 @@
 export { default as SearchIcon } from "@/assets/search.svg";
+export { default as HumidityIcon } from "@/assets/humidity.png";
+export { default as WindIcon } from "@/assets/wind.png";
+export { default as rain } from "@/assets/rain.png";
